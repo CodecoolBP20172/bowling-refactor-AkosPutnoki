02a23dd686a77424beebbf7eeb9e73f80861ac98
Akos Putnoki <putnokiakos@gmail.com>
2017-07-30 18:16:02 +0200

    Comments added

--- bowling.py
+++ bowling.py
@@ -6,10 +6,10 @@ def score(game):
     strike = 'xX'
     for i in range(len(game)):
         if game[i] in spare:
-            result += 10 - last + get_value(game[i+1])
+            result += 10 - last + get_value(game[i+1])  # if its a spare, adds 10 plus the value of the next throw to result 
         else:
             result += get_value(game[i])
-        if frame < 10  and game[i] in strike:
+        if frame < 10  and game[i] in strike:   # if its a strike, switches to the next frame and adds the value of the next 2 throw to result
             result += get_value(game[i+1])
             in_first_half = True
             frame += 1
@@ -25,7 +25,7 @@ def score(game):
         last = get_value(game[i])
     return result
 
-def get_value(char):   
+def get_value(char):    # converts strikes and spares to 10 and misses to 0, plus strings containing a number to integers   
     if char in [str(i) for i in range(1,10)]:
         return int(char)
     elif char in ['X', 'x', '/']:
