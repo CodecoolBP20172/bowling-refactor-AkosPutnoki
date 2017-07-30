6cfcea5814693b5a4f09c49c8e950ed5a14be51b
Akos Putnoki <putnokiakos@gmail.com>
2017-07-30 18:05:20 +0200

    Score function refactored

--- bowling.py
+++ bowling.py
@@ -2,36 +2,33 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
+    spare = '/'
+    strike = 'xX'
     for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
+        if game[i] in spare:
+            result += 10 - last + get_value(game[i+1])
         else:
             result += get_value(game[i])
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
-        if not in_first_half:
+        if frame < 10  and game[i] in strike:
+            result += get_value(game[i+1])
+            in_first_half = True
             frame += 1
-        if in_first_half == True:
+            if game[i+2] in spare:
+                result += 10 - get_value(game[i+1])
+            else:
+                result += get_value(game[i+2])
+        if in_first_half:
             in_first_half = False
         else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
             frame += 1
+            in_first_half = True
+        last = get_value(game[i])
     return result
 
 def get_value(char):   
     if char in [str(i) for i in range(1,10)]:
         return int(char)
-    elif char in ['x', 'X', '/']:
+    elif char in ['X', 'x', '/']:
         return 10
     elif char == '-':
         return 0
