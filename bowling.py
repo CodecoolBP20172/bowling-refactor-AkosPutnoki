8be42215b8abd165f40a2284e2feb4f420a2f786
Akos Putnoki <putnokiakos@gmail.com>
2017-07-30 15:55:29 +0200

    Get_value cleaned up

--- bowling.py
+++ bowling.py
@@ -28,14 +28,10 @@ def score(game):
             frame += 1
     return result
 
-def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+def get_value(char):   
+    if char in [str(i) for i in range(1,10)]:
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char in ['x', 'X', '/']:
         return 10
     elif char == '-':
         return 0
